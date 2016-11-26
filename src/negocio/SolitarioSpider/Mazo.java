4edea7c486a0158f113dbcf6d43b97fc77382bed
Ariel Daza <arieldc16@gmail.com>
2016-11-26 11:10:46 -0400

    logica de negocio

---
+++ src/negocio/SolitarioSpider/Mazo.java
@@ -0,0 +1,150 @@
+package negocio.SolitarioSpider;
+
+import java.util.ArrayList;
+
+/**
+ * @author Ariel Daza
+ * @version 1.0s
+ * @created 26-Nov.-2016 08:52:40 a.m.
+ */
+public class Mazo {
+
+    public ArrayList<Carta> cartas;
+
+    public Mazo() {
+        cartas = new ArrayList<>();
+    }
+
+    public boolean vacio() {
+        return cartas.isEmpty();
+    }
+
+    /**
+     *
+     * @param carta
+     */
+    public void addCarta(Carta carta) {
+        cartas.add(carta);
+    }
+
+    /**
+     *
+     * @param mazo
+     */
+    public void addCorte(Mazo mazo) {
+        for (Carta carta : mazo.getCartas()) {
+            this.addCarta(carta);
+        }
+    }
+
+    /**
+     *
+     * @param posicionCarta
+     * @return boolean
+     */
+    public boolean validarCorte(int posicionCarta) {
+        Carta aux = cartas.get(posicionCarta);
+        for (int i = posicionCarta + 1; i < cartas.size(); i++) {
+            Carta carta = cartas.get(i);
+            if (aux.getValor() != carta.getValor() + 1) {
+                return false;
+            }
+            aux = carta;
+        }
+        return true;
+    }
+
+    /**
+     *
+     * @param posicionCarta
+     * @return Mazo
+     */
+    public Mazo cortar(int posicionCarta) {
+        Mazo corte = new Mazo();
+        for (int i = posicionCarta; i < cartas.size(); i++) {
+            corte.addCarta(cartas.remove(i));
+        }
+        return corte;
+    }
+
+    public ArrayList<Carta> getCartas() {
+        return cartas;
+    }
+
+    public int cantCartasVisibles() {
+        return this.cartas.size() - this.cantCartasNoVisibles();
+    }
+
+    public int cantCartasNoVisibles() {
+        int cant = 0;
+        for (Carta carta : cartas) {
+            if (carta.esVisible()) {
+                cant++;
+            } else {
+                return cant;
+            }
+        }
+        return cant;
+    }
+
+    public boolean validarEscalera() {
+        if (cartas.size() > 12) {
+            Carta aux = cartas.get(cartas.size() - 1);
+            int size = cartas.size();
+            for (int i = 2; i < 14; i++) {
+                int pos = size - i;
+                Carta carta = cartas.get(pos);
+                if (aux.getValor() + 1 != carta.getValor()) {
+                    return false;
+                }
+                aux = carta;
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void removerEscalera() {
+        for (int i = 0; i < 13; i++) {
+            cartas.remove(cartas.size() - 1);
+        }
+    }
+
+    public Carta getBottom() {
+        if (!this.vacio()) {
+            return this.cartas.get(0);
+        }
+        return null;
+    }
+
+    public Carta getTop() {
+        if (!this.vacio()) {
+            return cartas.get(cartas.size());
+        }
+        return null;
+    }
+    
+    public Carta cortarTop(){
+        if (!this.vacio()) {
+            return cartas.remove(cartas.size()-1);
+        }
+        return null;
+    }
+
+    public void voltearTop() {
+        if (!this.vacio()) {
+            this.getTop().voltear();
+        }
+    }
+
+    public boolean validarAddCorte(Mazo mazo) {
+        if (!this.vacio()) {
+            if (this.getTop().getValor() != mazo.getBottom().getValor() + 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
---
+++ src/negocio/SolitarioSpider/SolitarioSpider.java
@@ -0,0 +1,155 @@
+package negocio.SolitarioSpider;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * @author Ariel Daza
+ * @version 1.0
+ * @created 26-Nov.-2016 08:52:40 a.m.
+ */
+public class SolitarioSpider {
+
+    public static final int INDEX_MAZO_BARAJA = 0;
+    public static final int INDEX_MAZO_UNO = 1;
+    public static final int INDEX_MAZO_DOS = 2;
+    public static final int INDEX_MAZO_TRES = 3;
+    public static final int INDEX_MAZO_CUATRO = 4;
+    public static final int INDEX_MAZO_CINCO = 5;
+    public static final int INDEX_MAZO_SEIS = 6;
+    public static final int INDEX_MAZO_SIETE = 7;
+    public static final int INDEX_MAZO_OCHO = 8;
+    public static final int INDEX_MAZO_NUEVE = 9;
+    public static final int INDEX_MAZO_DIEZ = 10;
+    public static final int NRO_MAZOS = 11;
+
+    private Mazo temp;
+    private int puntuacion;
+    private int indexTemp;
+    public ArrayList<Mazo> mazos;
+
+    public SolitarioSpider() {
+        int count = 0;
+        int[] valores = new int[104];
+        for (int i = 0; i < 104; i++) {
+            count++;
+            count = (count < 14) ? count : 1;
+            valores[i] = count;
+        }
+
+        shuffleArray(valores);
+
+        mazos = new ArrayList<>();
+
+        for (int i = 0; i < NRO_MAZOS; i++) {
+            mazos.add(new Mazo());
+        }
+
+        Mazo baraja = mazos.get(INDEX_MAZO_BARAJA);
+
+        for (int i = 0; i < 104; i++) {
+            Carta carta = new Carta(valores[i], Carta.TIPO_ESPADA);
+            baraja.addCarta(carta);
+        }
+
+        init();
+        for (int i = 0; i < 11; i++) {
+            System.out.println(mazos.get(i).toString());
+
+        }
+    }
+
+    public void clearTemporal() {
+        this.temp = null;
+        this.indexTemp = 0;
+    }
+
+    public void init() {
+        for (int i = 1; i < 6; i++) {
+            if (i == 4) {
+                distribuirCartas(INDEX_MAZO_UNO, INDEX_MAZO_CUATRO, false);
+                distribuirCartas(INDEX_MAZO_CINCO, INDEX_MAZO_DIEZ, true);
+            } else if (i == 5) {
+                distribuirCartas(INDEX_MAZO_UNO, INDEX_MAZO_CUATRO, true);
+            } else {
+                distribuirCartas(INDEX_MAZO_UNO, INDEX_MAZO_DIEZ, false);
+            }
+
+        }
+    }
+
+    public void repartirCartas() {
+        distribuirCartas(INDEX_MAZO_UNO, INDEX_MAZO_DIEZ, true);
+    }
+
+    /**
+     *
+     * @param mazoInicial
+     * @param mazoFinal
+     * @param visible
+     */
+    public void distribuirCartas(int mazoInicial, int mazoFinal, boolean visible) {
+        Mazo baraja = mazos.get(INDEX_MAZO_BARAJA);
+        if (!baraja.vacio()) {
+            for (int i = mazoInicial; i <= mazoFinal; i++) {
+                Carta carta = baraja.cortarTop();
+                if (carta != null) {
+                    if (visible) {
+                        carta.voltear();
+                    }
+                    Mazo mazo = mazos.get(i);
+                    mazo.addCarta(carta);
+                }
+            }
+        }
+    }
+
+    /**
+     *
+     * @param posicion
+     * @param nroMazo
+     */
+    public void cortarMazo(int posicion, int nroMazo) {
+        Mazo mazo = mazos.get(nroMazo);
+        if (mazo.validarCorte(posicion)) {
+            this.temp = mazo.cortar(posicion);
+            this.indexTemp = posicion;
+        }
+    }
+
+    /**
+     *
+     * @param nroMazo
+     */
+    public void mover(int mazoFinal) {
+        if (mazoFinal == indexTemp) {//caso 1 mover al mismo lugar
+            mazos.get(indexTemp).addCorte(temp);
+        } else {
+            Mazo aux = mazos.get(mazoFinal);
+            if (aux.validarAddCorte(aux)) {
+                mazos.get(mazoFinal).addCorte(temp);
+                mazos.get(indexTemp).voltearTop();
+            } else {
+                mazos.get(indexTemp).addCorte(temp);
+            }
+        }
+        clearTemporal();
+    }
+
+    public Mazo getTemp() {
+        return temp;
+    }
+
+    private void shuffleArray(int[] cartas) {
+
+        Random rnd = ThreadLocalRandom.current();
+        for (int i = cartas.length - 1; i > 0; i--) {
+            int index = rnd.nextInt(i + 1);
+            int a = cartas[index];
+            cartas[index] = cartas[i];
+            cartas[i] = a;
+        }
+    }
+
+}
